54898678f4adf35d74e127f560d3f3649562aae4
Juan Zepeda <juan.zero@unitec.edu>
2022-04-21 21:08:20 -0600

    Add project files.

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+
+//incluir libreria para salida de datos en consola
+#include <iostream>
+#include <conio.h>
+
+using std::cout;
+
+int main()
+{
+	//console output
+	cout << "Hello World! =)\n";
+	//cout << "Hola\t" << "mi nombre es" << "Juan Zepeda";
+
+	cout << "1\t0\t0\t0\n0\t1\t0\t0\n0\t0\t1\t0\n0\t0\t0\t1\n";
+
+	cout << "Lionel \"la pulga\" Messi\n";
+
+	_getch();
+}
